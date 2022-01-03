38c8e9b6f9efdd209472720bffc6a06ba1f1dea7
TDCIAN <tdcian71@gmail.com>
2022-01-04 08:35:07 +0900

    make BlogListCell

---
+++ DaumBlogApp/Entities/BlogListCellData.swift
@@ -0,0 +1,15 @@
+//
+//  BlogListCellData.swift
+//  DaumBlogApp
+//
+//  Created by JeongminKim on 2022/01/04.
+//
+
+import Foundation
+
+struct BlogListCellData {
+    let thumbnailURL: URL?
+    let name: String?
+    let title: String?
+    let datetime: Date?
+}
