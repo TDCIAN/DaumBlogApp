ce7d37b5cca6b8f952d299ac987c7c833071dda8
TDCIAN <tdcian71@gmail.com>
2022-01-05 07:39:49 +0900

    modify info file

--- DaumBlogApp/Entities/BlogListCellData.swift
+++ DaumBlogApp/Entities/BlogListCellData.swift
@@ -5,6 +5,8 @@
 //  Created by JeongminKim on 2022/01/04.
 //
 
+// bab264106e5e90a128c06fd3ea562238
+
 import Foundation
 
 struct BlogListCellData {
---
+++ DaumBlogApp/Entities/DaumKakaoBlog.swift
@@ -0,0 +1,8 @@
+//
+//  DaumKakaoBlog.swift
+//  DaumBlogApp
+//
+//  Created by JeongminKim on 2022/01/05.
+//
+
+import Foundation
